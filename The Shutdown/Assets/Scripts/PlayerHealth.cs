b807d9145f9f35387b907f1202457c0897caae4a
YourMom1997 <32456133+YourMom1997@users.noreply.github.com>
2019-01-21 14:04:07 +0100

    Healthbar almost finished

--- The Shutdown/Assets/Scripts/PlayerHealth.cs
+++ The Shutdown/Assets/Scripts/PlayerHealth.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int health;
 
+    public Image[] heartContainer;
+    public Image[] emptyHeartContainer;
+    public float fillAmount;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < emptyHeartContainer.Length; i++)
+        {
+            emptyHeartContainer[i].fillAmount = 0;
+        }
     }
 
     // Update is called once per frame
@@ -21,7 +29,11 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
-
+        for (int i = 0; i < 11; i = i+2) {
+            if (health == i) {
+                emptyHeartContainer[i/2].fillAmount =
+            }
+        }
         if (health <= 0)
         {
             Debug.Log("Dead");
