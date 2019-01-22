a22fa81e4e2f4119e4622706fff020d4e0d5be9a
YourMom1997 <32456133+YourMom1997@users.noreply.github.com>
2019-01-22 13:32:46 +0100

    revert to Health to fix the scene

--- The Shutdown/Assets/Scripts/PlayerHealth.cs
+++ The Shutdown/Assets/Scripts/PlayerHealth.cs
@@ -28,25 +28,15 @@ public class PlayerHealth : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
-        //takes damage
         health -= damage;
-        //loop to check which array number corresponds to current health
-        for (int i = 0; i < 10; i = i+2) {
-            //checks if whole hearts need to be taken
-            if (health == i)
-            {
-                emptyHeartContainer[i/2].fillAmount = 1;
-            }
-            //checks if half hearts need to be taken
-            else if (health > i && health < i+2)
-            {
-                emptyHeartContainer[i/2].fillAmount = 0.5f;
+        for (int i = 0; i < 11; i = i+2) {
+            if (health == i) {
+                emptyHeartContainer[i/2].fillAmount =
             }
         }
         if (health <= 0)
         {
             Debug.Log("Dead");
-            health = 0;
         }
     }
 }
